76b01e3ebc59995e35b0ba104a607205bceb494d
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2025-09-19 08:29:44 +0700

    .

--- flow/001/8-GASOHR001.js
+++ flow/001/8-GASOHR001.js
@@ -515,6 +515,8 @@ router.post('/GASOHR001-feedback', async (req, res) => {
         for (i = 0; i < feedback[0]['CHECKlist'].length; i++) {
           if (input["ITEMs"] === feedback[0]['CHECKlist'][i]['key']) {
             feedback[0]['CHECKlist'][i]['FINISH'] = 'OK';
+            feedback[0]['CHECKlist'][i]['timestamp'] = `${Date.now()}`;
+            feedback[0]['CHECKlist'][i]['ID'] = ``;
             // console.log(feedback[0]['CHECKlist']);
             let feedbackupdate = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'CHECKlist': feedback[0]['CHECKlist'] } });
             break;
