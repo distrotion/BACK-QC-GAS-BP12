8d84dd69ed5a4c51e0264a5e5474f8bd64b9aa02
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2026-04-20 11:10:27 +0700

    .

--- flow/001/3-GASHMV002.js
+++ flow/001/3-GASHMV002.js
@@ -565,6 +565,122 @@ router.post('/GASHMV002-feedback', async (req, res) => {
 
           } else if (masterITEMs[0]['RESULTFORMAT'] === 'Graph') {
 
+            // if (GASHMV002db['GRAPHTYPE'] == 'CDE') {
+
+            //   //
+            //   let axis_data = [];
+            //   for (i = 0; i < LISTbuffer.length; i++) {
+            //     if (LISTbuffer[i]['PO1'] !== 'Mean') {
+            //       axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
+            //     }
+            //   }
+            //   //-----------------core
+
+            //   let core = 0;
+            //   if (GASHMV002db['INTERSEC'] !== '') {
+            //     core = parseFloat(GASHMV002db['INTERSEC'])
+            //   } else {
+            //     core = parseFloat(axis_data[axis_data.length - 1]['y'])
+            //   }
+
+            //   //-----------------core
+            //   let RawPoint = [];
+            //   for (i = 0; i < axis_data.length - 1; i++) {
+            //     if (core <= axis_data[i].y && core >= axis_data[i + 1].y) {
+            //       RawPoint.push({ Point1: axis_data[i], Point2: axis_data[i + 1] });
+            //       break
+            //     }
+            //   }
+
+            //   try {
+            //     let pointvalue = RawPoint[0].Point2.x - RawPoint[0].Point1.x;
+            //     let data2 = RawPoint[0].Point1.y - core;
+            //     let data3 = RawPoint[0].Point1.y - RawPoint[0].Point2.y;
+
+            //     let RawData = RawPoint[0].Point1.x + (data2 / data3 * pointvalue);
+            //     let graph_ans_X = parseFloat(RawData.toFixed(2));
+
+            //     feedback[0]['FINAL_ANS'][input["ITEMs"]] = graph_ans_X;
+            //     feedback[0]['FINAL_ANS'][`${input["ITEMs"]}_point`] = { "x": graph_ans_X, "y": core };
+
+            //     let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+            //   }
+            //   catch (err) {
+            //     GASHMV002db[`INTERSEC_ERR`] = 1;
+            //   }
+
+            //   //
+            // } else if (GASHMV002db['GRAPHTYPE'] == 'CDE') {
+            //   let axis_data = [];
+            //   for (i = 0; i < LISTbuffer.length; i++) {
+            //     if (LISTbuffer[i]['PO1'] !== 'Mean') {
+            //       axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
+            //     }
+            //   }
+
+            //   let d = []
+            //   for (i = 0; i < axis_data.length - 1; i++) {
+            //     d.push((axis_data[i].y - axis_data[i + 1].y) / (axis_data[i + 1].x - axis_data[i].x));
+            //   }
+
+            //   let def = []
+
+            //   for (i = 0; i < d.length - 1; i++) {
+            //     if (d[i] > d[i + 1]) {
+            //       def[i] = (d[i] - d[i + 1])
+            //     } else {
+            //       def[i] = (d[i + 1] - d[i])
+            //     }
+
+            //   }
+
+            //   for (j = 0; j < def.length; j++) {
+            //     if (def[j] === Math.max(...def)) {
+            //       pos = [j + 1, j + 2]
+            //     }
+            //   }
+
+            //   let d1 = -d[pos[0] - 1]
+            //   let d2 = -d[pos[1]]
+
+
+            //   let c1 = (axis_data[pos[0]].y - d1 * axis_data[pos[0]].x);
+            //   let c2 = (axis_data[pos[1]].y - d2 * axis_data[pos[1]].x);
+
+
+            //   let Xans = 0;
+            //   let Yans = 0;
+            //   let x = (c[1] - c[0]) / (d1 - d2);
+
+
+            //   if (x >= 0) {
+            //     Xans = x
+            //   } else {
+            //     Xans = -x
+            //   }
+
+            //   y = d1 * Xans + c[0]
+            //   Yans = y
+
+            //   let graph_ans_X = parseFloat(Xans.toFixed(2));
+            //   let graph_ans_Y = parseFloat(Yans.toFixed(2));
+
+            //   feedback[0]['FINAL_ANS'][input["ITEMs"]] = graph_ans_X;
+            //   feedback[0]['FINAL_ANS'][`${input["ITEMs"]}_point`] = { "x": graph_ans_X, "y": graph_ans_Y };
+
+            //   let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+
+
+            // } else {
+            //   let dataCheck = await axios.post("http://localhost:16180/GRAPH-recal", {
+            //     "PO": GASHMV002db["PO"],
+            //     "MODE": "CDE",
+            //     "ITEMs": "ITEMs-5f19aaa2fe12be0020dbd3c2",
+            //     "NAME_INS": "GAS-HMV-002",
+            //     "INTERSEC": ""
+            //   })
+            // }
+
             if (GASHMV002db['GRAPHTYPE'] == 'CDE') {
 
               //
@@ -610,75 +726,216 @@ router.post('/GASHMV002-feedback', async (req, res) => {
               }
 
               //
-            } else if (GASHMV002db['GRAPHTYPE'] == 'CDE') {
+            } else if (GASHMV002db['GRAPHTYPE'] == 'CDT') {
+
+              //
               let axis_data = [];
               for (i = 0; i < LISTbuffer.length; i++) {
                 if (LISTbuffer[i]['PO1'] !== 'Mean') {
                   axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
                 }
               }
+              //-----------------core
 
-              let d = []
-              for (i = 0; i < axis_data.length - 1; i++) {
-                d.push((axis_data[i].y - axis_data[i + 1].y) / (axis_data[i + 1].x - axis_data[i].x));
-              }
-
-              let def = []
-
-              for (i = 0; i < d.length - 1; i++) {
-                if (d[i] > d[i + 1]) {
-                  def[i] = (d[i] - d[i + 1])
-                } else {
-                  def[i] = (d[i + 1] - d[i])
-                }
-
-              }
-
-              for (j = 0; j < def.length; j++) {
-                if (def[j] === Math.max(...def)) {
-                  pos = [j + 1, j + 2]
-                }
-              }
-
-              let d1 = -d[pos[0] - 1]
-              let d2 = -d[pos[1]]
-
-
-              let c1 = (axis_data[pos[0]].y - d1 * axis_data[pos[0]].x);
-              let c2 = (axis_data[pos[1]].y - d2 * axis_data[pos[1]].x);
-
-
-              let Xans = 0;
-              let Yans = 0;
-              let x = (c[1] - c[0]) / (d1 - d2);
-
-
-              if (x >= 0) {
-                Xans = x
+              let core = 0;
+              if (GASHMV002db['INTERSEC'] !== '') {
+                core = parseFloat(GASHMV002db['INTERSEC'])
               } else {
-                Xans = -x
+                // core = parseFloat(axis_data[axis_data.length - 1]['y']) 
+                core = parseFloat(axis_data[axis_data.length - 1]['y']) + 50
               }
 
-              y = d1 * Xans + c[0]
-              Yans = y
+              //-----------------core
+              let RawPoint = [];
+              for (i = 0; i < axis_data.length - 1; i++) {
+                if (core <= axis_data[i].y && core >= axis_data[i + 1].y) {
+                  RawPoint.push({ Point1: axis_data[i], Point2: axis_data[i + 1] });
+                  break
+                }
+              }
 
-              let graph_ans_X = parseFloat(Xans.toFixed(2));
-              let graph_ans_Y = parseFloat(Yans.toFixed(2));
+              try {
+                let pointvalue = RawPoint[0].Point2.x - RawPoint[0].Point1.x;
+                let data2 = RawPoint[0].Point1.y - core;
+                let data3 = RawPoint[0].Point1.y - RawPoint[0].Point2.y;
 
-              feedback[0]['FINAL_ANS'][input["ITEMs"]] = graph_ans_X;
-              feedback[0]['FINAL_ANS'][`${input["ITEMs"]}_point`] = { "x": graph_ans_X, "y": graph_ans_Y };
+                let RawData = RawPoint[0].Point1.x + (data2 / data3 * pointvalue);
+                let graph_ans_X = parseFloat(RawData.toFixed(2));
 
-              let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+                feedback[0]['FINAL_ANS'][input["ITEMs"]] = graph_ans_X;
+                feedback[0]['FINAL_ANS'][`${input["ITEMs"]}_point`] = { "x": graph_ans_X, "y": core };
 
+                let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+              }
+              catch (err) {
+                GASHMV002db[`INTERSEC_ERR`] = 1;
+              }
 
-            } else {
-              let dataCheck = await axios.post("http://localhost:16180/GRAPH-recal", {
-                "PO": GASHMV002db["PO"],
-                "MODE": "CDE",
-                "ITEMs": "ITEMs-5f19aaa2fe12be0020dbd3c2",
+              //
+            } else if (GASHMV002db['GRAPHTYPE'] == 'CDT(S)') {
+
+              //
+              let axis_data = [];
+              for (i = 0; i < LISTbuffer.length; i++) {
+                if (LISTbuffer[i]['PO1'] !== 'Mean') {
+                  axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
+                }
+              }
+              //-----------------core
+
+              // let core = 0;
+              // if (GASHMV002db['INTERSEC'] !== '') {
+              //   core = parseFloat(GASHMV002db['INTERSEC'])
+              // } else {
+              core = parseFloat(axis_data[axis_data.length - 1]['y']) + parseFloat(GASHMV002db['INTERSEC'])
+              // }
+
+              //-----------------core
+              let inputSP = {
+                "PO": GASHMV002db[`PO`],
+                "ITEMs": GASHMV002db[`inspectionItem`],
                 "NAME_INS": "GAS-HMV-002",
-                "INTERSEC": ""
-              })
+                "INTERSEC": core,
+              }
+
+              if (inputSP["PO"] !== undefined && inputSP["ITEMs"] !== undefined && inputSP["NAME_INS"] !== undefined && inputSP["INTERSEC"] !== undefined) {
+                let feedback = await mongodb.find(MAIN_DATA, MAIN, { "PO": inputSP['PO'] });
+                console.log(feedback[0]['FINAL'][inputSP["NAME_INS"]][inputSP["ITEMs"]])
+                if (feedback.length > 0 && feedback[0]['FINAL'] != undefined && feedback[0]['FINAL'][inputSP["NAME_INS"]] != undefined && feedback[0]['FINAL'][inputSP["NAME_INS"]][inputSP["ITEMs"]] != undefined) {
+                  // console.log(Object.keys(feedback[0]['FINAL'][NAME_INS][inputSP["ITEMs"]]));
+                  let oblist = Object.keys(feedback[0]['FINAL'][inputSP["NAME_INS"]][inputSP["ITEMs"]]);
+                  let ob = feedback[0]['FINAL'][inputSP["NAME_INS"]][inputSP["ITEMs"]];
+
+                  let LISTbuffer = [];
+                  let ITEMleftVALUEout = [];
+
+                  for (i = 0; i < oblist.length; i++) {
+                    LISTbuffer.push(...ob[oblist[i]])
+                  }
+
+
+
+
+                  //
+                  let axis_data = [];
+                  for (i = 0; i < LISTbuffer.length; i++) {
+                    if (LISTbuffer[i]['PO1'] !== 'Mean') {
+                      axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
+                    }
+                  }
+                  //-----------------core
+
+                  let core = 0;
+
+                  // if (inputSP['INTERSEC'] !== '') {
+                  //   core = parseFloat(inputSP['INTERSEC'])
+                  // } else {
+                  core = parseFloat(axis_data[axis_data.length]['y']) + parseFloat(inputSP['INTERSEC'])
+                  // }
+
+                  console.log(parseFloat(axis_data[axis_data.length]['y']) + parseFloat(inputSP['INTERSEC']));
+                  //-----------------core
+                  let RawPoint = [];
+                  console.log(LISTbuffer);
+                  for (i = 0; i < axis_data.length - 1; i++) {
+                    if (core <= axis_data[i].y && core >= axis_data[i + 1].y) {
+                      RawPoint.push({ Point1: axis_data[i], Point2: axis_data[i + 1] });
+                      break
+                    }
+                  }
+                  console.log(LISTbuffer);
+                  console.log(axis_data);
+                  console.log(RawPoint);
+
+                  try {
+                    let pointvalue = RawPoint[0].Point2.x - RawPoint[0].Point1.x;
+                    let data2 = RawPoint[0].Point1.y - core;
+                    let data3 = RawPoint[0].Point1.y - RawPoint[0].Point2.y;
+
+                    let RawData = RawPoint[0].Point1.x + (data2 / data3 * pointvalue);
+                    let graph_ans_X = parseFloat(RawData.toFixed(2));
+                    console.log("-----------")
+                    feedback[0]['FINAL_ANS'][inputSP["ITEMs"]] = graph_ans_X;
+                    feedback[0]['FINAL_ANS'][`${inputSP["ITEMs"]}_point`] = { "x": graph_ans_X, "y": core };
+
+                    let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": inputSP['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+                  }
+                  catch (err) {
+                    // TPGHMV002db[`INTERSEC_ERR`] = 1;
+                  }
+
+
+
+                }
+
+              }
+
+              //
+            } else {
+              try {
+                let axis_data = [];
+                for (i = 0; i < LISTbuffer.length; i++) {
+                  if (LISTbuffer[i]['PO1'] !== 'Mean') {
+                    axis_data.push({ x: parseFloat(LISTbuffer[i].PO8), y: parseFloat(LISTbuffer[i].PO3) });
+                  }
+                }
+
+                let d = []
+                for (i = 0; i < axis_data.length - 1; i++) {
+                  d.push((axis_data[i].y - axis_data[i + 1].y) / (axis_data[i + 1].x - axis_data[i].x));
+                }
+
+                let def = []
+
+                for (i = 0; i < d.length - 1; i++) {
+                  if (d[i] > d[i + 1]) {
+                    def[i] = (d[i] - d[i + 1])
+                  } else {
+                    def[i] = (d[i + 1] - d[i])
+                  }
+
+                }
+
+                for (j = 0; j < def.length; j++) {
+                  if (def[j] === Math.max(...def)) {
+                    pos = [j + 1, j + 2]
+                  }
+                }
+
+                let d1 = -d[pos[0] - 1]
+                let d2 = -d[pos[1]]
+
+
+                let c1 = (axis_data[pos[0]].y - d1 * axis_data[pos[0]].x);
+                let c2 = (axis_data[pos[1]].y - d2 * axis_data[pos[1]].x);
+
+
+                let Xans = 0;
+                let Yans = 0;
+                let x = (c[1] - c[0]) / (d1 - d2);
+
+
+                if (x >= 0) {
+                  Xans = x
+                } else {
+                  Xans = -x
+                }
+
+                y = d1 * Xans + c[0]
+                Yans = y
+
+                let graph_ans_X = parseFloat(Xans.toFixed(2));
+                let graph_ans_Y = parseFloat(Yans.toFixed(2));
+
+                feedback[0]['FINAL_ANS'][input["ITEMs"]] = graph_ans_X;
+                feedback[0]['FINAL_ANS'][`${input["ITEMs"]}_point`] = { "x": graph_ans_X, "y": graph_ans_Y };
+
+                let feedbackupdateRESULTFORMAT = await mongodb.update(MAIN_DATA, MAIN, { "PO": input['PO'] }, { "$set": { 'FINAL_ANS': feedback[0]['FINAL_ANS'] } });
+
+              }
+              catch (err) {
+                GASHMV002db[`INTERSEC_ERR`] = 1;
+              }
             }
 
           } else if (masterITEMs[0]['RESULTFORMAT'] === 'Picture') {
